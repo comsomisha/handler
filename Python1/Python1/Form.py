5d823e11fb5e535f3a47e6177fa417e1fcb24bd4
misha <shmelev1996@mail.ru>
2016-12-01 15:20:14 +0300

    In process 2

--- Python1/Python1/Form.py
+++ Python1/Python1/Form.py
@@ -16,9 +16,7 @@ class Handler:
       pixels = {}
       paint = Image.open(image)
       paint = paint.convert("P")
-      pixels = paint.histogram()
-      return pixels   
-   pixels = ConvertToPixel(image)
+      return paint  
 root = Tk()
 Handler(root)   
    # def GetNumbers():
