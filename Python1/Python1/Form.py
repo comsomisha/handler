940e32991d520908d1bdec6eb6312074214e0fc6
misha <shmelev1996@mail.ru>
2016-12-01 12:43:10 +0300

    In process

--- Python1/Python1/Form.py
+++ Python1/Python1/Form.py
@@ -1,8 +1,16 @@
 from tkinter import *
 class Handler:
-   start = Tk()
-   start.title("Number recognition")
-   start.geometry('300x300')
+   def _init_(self,start):
+    self.start = start
+    self.start.title("Number recognition")
+    self.start.geometry('300x300')
+    self.OpenButton = Button(self.start, text = 'Open File', command = self.ConvertToPixels)
+    self.OpenButton.pack(side = BOTTOM)
+    self.Result = Text(font=('times',12),width=15,height=15)
+    self.Result.pack(side = LEFT, fill=BOTH)
+    self.Setting = Text(font=('times',12),width=15,height=15)
+    self.Setting.pack(side = LEFT, fill=BOTH)
+    self.start.mainloop()
    def ConvertToPixels(image):
       from PIL import Image
       pixels = {}
@@ -11,6 +19,8 @@ class Handler:
       pixels = paint.histogram()
       return pixels   
    pixels = ConvertToPixel(image)
+root = Tk()
+Handler(root)   
    # def GetNumbers():
    # def CompareNumbers(numbers,usernumbers):
 
